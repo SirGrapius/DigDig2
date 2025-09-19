aec7710568b24af7e40badb0cf659327ea07866d
Anton Vahlenberg <anton.vahlenberg@learnet.se>
2025-09-19 10:03:52 +0200

    Plants can aim at nearest enemy

--- DigDig2/Assets/Scripts/PlantScript.cs
+++ DigDig2/Assets/Scripts/PlantScript.cs
@@ -9,31 +9,34 @@ public class PlantScript : MonoBehaviour
         
     void Update()
     {
-        targetPos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-        targetPos.x = targetPos.x - transform.position.x;
-        targetPos.y = targetPos.y - transform.position.y;
-        transform.rotation = Quaternion.Euler(0,0,Mathf.Atan2(targetPos.y, targetPos.x) * Mathf.Rad2Deg);
         time += Time.deltaTime;
         if (time >= attackFrequency)
         {
             time = 0;
-            Attack();
+            targetPos = Attack().transform.position;
+            targetPos.x = targetPos.x - transform.position.x;
+            targetPos.y = targetPos.y - transform.position.y;
         }
+        transform.rotation = Quaternion.Euler(0, 0, Mathf.Atan2(targetPos.y, targetPos.x) * Mathf.Rad2Deg);
     }
-    void Attack()
+
+    GameObject Attack()
     {
         enemies = GameObject.FindGameObjectsWithTag("Enemy");
+        
         for (int i = 0; i < enemies.Length; i++)
         {
+            Vector2 v0 = enemies[0].transform.position;
+            v0.x -= transform.position.x;
+            v0.y -= transform.position.y;
             Vector2 v = enemies[i].transform.position;
             v.x -= transform.position.x;
             v.y -= transform.position.y;
-            v.x = Mathf.Abs(v.x);
-            v.y = Mathf.Abs(v.y);
-            if (v.sqrMagnitude < enemies[i].transform.position.sqrMagnitude)
+            if (v.sqrMagnitude < v0.sqrMagnitude)
             {
-
+                enemies[0] = enemies[i];
             }
         }
+        return enemies[0];
     }
 }
