8b0ab03bd27b55d10b4b4253ccf8e59926157ddf
Adonai <adonai.osterberg@elev.ga.lbs.se>
2025-11-07 12:25:10 +0100

    scene manager, screen fade, button inflation

---
+++ DigDig2/Assets/Scripts/Menu Scripts/ButtonInflation.cs
@@ -0,0 +1,46 @@
+using UnityEngine;
+
+public class ButtonInflation : MonoBehaviour
+{
+    [SerializeField] bool inflate;
+    [SerializeField] float lerpAmount;
+    [SerializeField] AudioClip myClip;
+
+    [SerializeField] AudioSource source;
+    [SerializeField] Vector2 enterSize;
+    [SerializeField] Vector2 originalSize;
+    RectTransform myTransform;
+
+    void Start()
+    {
+        myTransform = GetComponent<RectTransform>();
+        enterSize.x = myTransform.localScale.x * 1.1f;
+        enterSize.y = myTransform.localScale.y * 1.1f;
+        originalSize = myTransform.localScale;
+        source = GetComponentInParent<AudioSource>();
+        source.clip = myClip;
+    }
+
+
+    void Update()
+    {
+        if (inflate)
+        {
+            myTransform.localScale = Vector2.Lerp(enterSize, myTransform.localScale, Mathf.Pow(0.5f, Time.deltaTime * lerpAmount));
+        }
+        else
+        {
+            myTransform.localScale = Vector2.Lerp(originalSize, myTransform.localScale, Mathf.Pow(0.5f, Time.deltaTime * lerpAmount));
+        }
+    }
+
+    public void MouseEnter()
+    {
+        inflate = true;
+        source.Play();
+    }
+    public void MouseExit()
+    {
+        inflate = false;
+    }
+}
