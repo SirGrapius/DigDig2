8b7d6fef431296c062337ba320e41d67bf34a02f
Anton Vahlenberg <anton.vahlenberg@learnet.se>
2026-01-16 12:07:39 +0100

    how dumb can a single person be

--- DigDig2/Assets/Scripts/PlantScripts/Chiliscript.cs
+++ DigDig2/Assets/Scripts/PlantScripts/Chiliscript.cs
@@ -26,6 +26,7 @@ public class Chiliscript : MonoBehaviour
 
     private void Awake()
     {
+        Death = GetComponent<PlantDeath>();
         baseAnimator = GetComponent<Animator>();
         targeting = GetComponent<ClosestEnemy>();
         growing = true;
@@ -82,7 +83,7 @@ public class Chiliscript : MonoBehaviour
                 }
             }
         }
-        if (baseAnimator.GetBool("attack"))
+        if (baseAnimator.GetBool("Attack"))
         {
             attackTimer += Time.deltaTime;
             if (attackTimer >= attackAnim.length)
